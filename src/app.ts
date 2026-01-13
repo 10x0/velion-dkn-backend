1c4aef2d7860d6a47ba3831318147a6bc300c426
10x0 <aasseess@pm.me>
2026-01-13 18:31:51 +0000

    fix: CORS error

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,13 @@ import userRoutes from './routes/userRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(express.json());
 
 app.use('/api/search', searchRoutes);
